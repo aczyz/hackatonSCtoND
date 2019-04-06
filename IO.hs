a6127f378a8d574cd323ac56c1468d52a1356ad2
mgawek <42947381+mgawek@users.noreply.github.com>
2019-04-06 08:20:56 +0200

    Update IO.hs

--- IO.hs
+++ IO.hs
@@ -1,6 +1,7 @@
 import Data.Char
 import System.IO
 import LambdaLadies
+{-
 
 strlen :: IO ()
 strlen = do putStr "Wybierz regułę w rachunku sekwentów: \n 1 : lewa koniunkcja \n 2 : prawa koniunkcja \n 3 : lewa alternatywa \n 4 : prawa alternatywa \n 5 : lewa implikacja \n 6 : prawa implikacja \n 7 : falsum \n Wybierz formułę (podaj liczbę): "
@@ -9,8 +10,7 @@ strlen = do putStr "Wybierz regułę w rachunku sekwentów: \n 1 : lewa koniunkc
             if xs == "1" then
                 putStr "The string has "
             else putStrLn (show (cIntr p1) :: String)
-=======
-{-            putStr "The string has "
+             putStr "The string has "
             putStr (show (length xs))
             putStrLn " characters"
 -}
@@ -20,8 +20,9 @@ main = do
     putStrLn (" Wybierz regułę w rachunku sekwentów: \n 1 : lewa koniunkcja \n 2 : prawa koniunkcja \n 3 : lewa alternatywa \n 4 : prawa alternatywa \n 5 : lewa implikacja \n 6 : prawa implikacja \n 7 : falsum \n Wybierz formułę (podaj liczbę): ")
     xs <- getLine
     let xs2 = trans xs
+    let xs3 = f2 xs
     putStrLn ("wybrałeś " ++ xs2)
-    putStrLn "tu sie wyswietli w rach sekwentow"
+    putStrLn ("Twoja formuła w rachunku sekwentów ma postać:" ++ f2 xs)
 
 
 trans :: String -> String
@@ -33,4 +34,15 @@ trans x = case x of
         "5" -> "lewą implikację"
         "6" -> "prawą implikację"
         "7" -> "falsum"
->>>>>>> 173c9a9bf2cc7e671bd8b8272bf517e44a1c9fae
+
+f2 :: String -> String
+f2 x = case x of
+  "1" -> "([([V 1, V 2], [N])], ([C (V 1) (V 2)], [N]))"
+  "2" -> "([([V 1, V 2], [N])], ([C (V 1) (V 2)], [N]))"
+  "3" -> "([([V 1, V 2], [N])], ([C (V 1) (V 2)], [N]))"
+  "4" -> "([([V 1, V 2], [N])], ([C (V 1) (V 2)], [N]))"
+  "5" -> "([([V 1, V 2], [N])], ([C (V 1) (V 2)], [N]))"
+  "6" -> "([([V 1, V 2], [N])], ([C (V 1) (V 2)], [N]))"
+  "7" -> "([([V 1, V 2], [N])], ([C (V 1) (V 2)], [N]))"
+
+
