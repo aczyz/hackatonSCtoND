cc94e5833ed92ba352a2345ac2b5487d7bcc2468
Kinga Ordecka <kingaordecka@gmail.com>
2019-04-06 09:36:26 +0200

    integration

--- IO.hs
+++ IO.hs
@@ -5,11 +5,9 @@ import LambdaLadies
 strlen :: IO ()
 strlen = do putStr "Wybierz regułę w rachunku sekwentów: \n 1 : lewa koniunkcja \n 2 : prawa koniunkcja \n 3 : lewa alternatywa \n 4 : prawa alternatywa \n 5 : lewa implikacja \n 6 : prawa implikacja \n 7 : falsum \n Wybierz formułę (podaj liczbę): "
             xs <- getLine
-<<<<<<< HEAD
             if xs == "1" then
                 putStr "The string has "
             else putStrLn (show (cIntr p1) :: String)
-=======
 {-            putStr "The string has "
             putStr (show (length xs))
             putStrLn " characters"
@@ -33,4 +31,3 @@ trans x = case x of
         "5" -> "lewą implikację"
         "6" -> "prawą implikację"
         "7" -> "falsum"
->>>>>>> 173c9a9bf2cc7e671bd8b8272bf517e44a1c9fae
