e81ac449c14d991af570fe49472599a9d96f8e07
Kinga Ordecka <kingaordecka@gmail.com>
2019-04-06 08:04:16 +0200

    progress :)

--- IO.hs
+++ IO.hs
@@ -1,9 +1,10 @@
+import Data.Char
 import System.IO
+import LambdaLadies
 
 strlen :: IO ()
 strlen = do putStr "Wybierz regułę w rachunku sekwentów: \n 1 : lewa koniunkcja \n 2 : prawa koniunkcja \n 3 : lewa alternatywa \n 4 : prawa alternatywa \n 5 : lewa implikacja \n 6 : prawa implikacja \n 7 : falsum \n Wybierz formułę (podaj liczbę): "
             xs <- getLine
-{-            putStr "The string has "
-            putStr (show (length xs))
-            putStrLn " characters"
--}
+            if xs == "1" then
+                putStr "The string has "
+            else putStrLn (show (cIntr p1) :: String)
